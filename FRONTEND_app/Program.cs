999fde479ab7f876f64e046eb74a7d58fd17fdb5
mateusz <mateusz.j515@gmail.com>
2024-09-04 13:55:39 +0200

    Add additional display information

--- FRONTEND_app/Program.cs
+++ FRONTEND_app/Program.cs
@@ -21,7 +21,6 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services
     .AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<ApiService>();
 builder.Services.AddHttpClient();
 builder.Services.AddBlazorBootstrap();
